3c4adbc1f79c16faeb664e90f2b4b4fa94424620
guptajiakshat <akshatgupta2k@gmail.com>
2022-01-05 18:16:20 +0530

    11.6

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -8,52 +8,71 @@ mongoose.connect(connectionURL, {
 const User = mongoose.model('User', {
     name: {
         type: String,
-        required: true
-    },
-    age: {
-        type: Number,
-        validate(value) {
-            if (value < 0)
-                throw new Error('Age must be a positive number')
-        }
+        required: true,
+        trim: true
     },
     email: {
         type: String,
         required: true,
+        trim: true,
+        lowercase: true,
         validate(value) {
-            if (!validator.isEmail(value))
+            if (!validator.isEmail(value)) {
                 throw new Error('Email is invalid')
+            }
+        }
+    },
+    age: {
+        type: Number,
+        default: 0,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Age must be a postive number')
+            }
+        }
+    },
+    passoword: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 7,
+        validate(value) {
+            if (value.toLowerCase().includes("password"))
+                throw new Error('error')
         }
     }
 })
 
-const me = new User({
-    name: 'mike',
-    email: 'mike@gaf.com'
-})
-
-me.save().then(() => {
-    console.log(me)
-}).catch((error) => {
-    console.log(error)
-})
-
-// const Task = mongoose.model('Tasks', {
-//     description: {
-//         type: String
-//     },
-//     completed: {
-//         type: Boolean
-//     }
+// const me = new User({
+//     name: 'mike',
+//     email: 'mike@gaf.com',
+//     passoword: "adslfaksdjf839q4u~"
 // })
 
-// const task = new Task({
-//     description: 'Learn mongoose',
-//     completed: true
-// })
-
-// task.save().then(() => {
-//     console.log(task)
+// me.save().then(() => {
+//     console.log(me)
 // }).catch((error) => {
 //     console.log(error)
 // })
+
+const Task = mongoose.model('Tasks', {
+    description: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    }
+})
+
+const task = new Task({
+    description: '       Learn mongoose       '
+})
+
+task.save().then(() => {
+    console.log(task)
+}).catch((error) => {
+    console.log(error)
+})
