31f50b2fe4c3dfce3e464cf03f3e9063b1b380bf
guptajiakshat <akshatgupta2k@gmail.com>
2022-01-10 22:54:57 +0530

    idk

--- src/models/user.js
+++ src/models/user.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
-const userSchema = mongoose.Schema({
+const userSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
@@ -20,6 +21,17 @@ const userSchema = mongoose.Schema({
             }
         }
     },
+    password: {
+        type: String,
+        required: true,
+        minlength: 7,
+        trim: true,
+        validate(value) {
+            if (value.toLowerCase().includes('password')) {
+                throw new Error('Password cannot contain "password"')
+            }
+        }
+    },
     age: {
         type: Number,
         default: 0,
@@ -29,42 +41,48 @@ const userSchema = mongoose.Schema({
             }
         }
     },
-    password: {
-        type: String,
-        required: true,
-        trim: true,
-        minlength: 7,
-        validate(value) {
-            if (value.toLowerCase().includes("password"))
-                throw new Error('error')
+    tokens: [{
+        token: {
+            type: String,
+            required: true
         }
-    }
+    }]
 })
-
-
-
-userSchema
 
 userSchema.statics.findByCredentials = async (email, password) => {
     const user = await User.findOne({ email })
     if (!user) {
-        throw new Error('unable to login')
+        throw new Error('Unable to login')
     }
-    const isMatched = await bcrypt.compare(password, user.password)
-    if (!isMatched) {
-        throw new Error('unable to login')
+
+    const isMatch = await bcrypt.compare(password, user.password)
+
+    if (!isMatch) {
+        throw new Error('Unable to login')
     }
+
     return user
 }
 
-// hash the plain text password
+userSchema.methods.generateAuthToken = async function () {
+    const user = this
+    const token = jwt.sign({ _id: user._id.toString() }, 'randomToken')
+    user.tokens = user.tokens.concat({ token })
+    await user.save()
+    return token
+}
+
+// Hash the plain text password before saving
 userSchema.pre('save', async function (next) {
     const user = this
+
     if (user.isModified('password')) {
-        user.password = await bcrypt.hash(user.password, 10)
+        user.password = await bcrypt.hash(user.password, 8)
     }
+
     next()
 })
 
 const User = mongoose.model('User', userSchema)
+
 module.exports = User
--- src/router/user.js
+++ src/router/user.js
@@ -1,14 +1,14 @@
-const express = require('express');
+const express = require('express')
 const User = require('../models/user')
-const router = new express.Router()
-
-// router.
+const router = express.Router()
 
 router.post('/users', async (req, res) => {
     const user = new User(req.body)
+
     try {
         await user.save()
-        res.status(201).send(user)
+        const token = await user.generateAuthToken()
+        res.status(201).send({ user, token })
     } catch (e) {
         res.status(400).send(e)
     }
@@ -17,9 +17,10 @@ router.post('/users', async (req, res) => {
 router.post('/users/login', async (req, res) => {
     try {
         const user = await User.findByCredentials(req.body.email, req.body.password)
-        res.send(user)
+        const token = await user.generateAuthToken()
+        res.send({ user, token })
     } catch (e) {
-        res.status(400).send()
+        res.status(400).send(e)
     }
 })
 
@@ -27,19 +28,23 @@ router.get('/users', async (req, res) => {
     try {
         const users = await User.find({})
         res.send(users)
-    } catch {
+    } catch (e) {
         res.status(500).send()
     }
 })
 
 router.get('/users/:id', async (req, res) => {
     const _id = req.params.id
+
     try {
-        const user = await User.find(_id)
-        if (!user)
+        const user = await User.findById(_id)
+
+        if (!user) {
             return res.status(404).send()
+        }
+
         res.send(user)
-    } catch {
+    } catch (e) {
         res.status(500).send()
     }
 })
@@ -47,15 +52,22 @@ router.get('/users/:id', async (req, res) => {
 router.patch('/users/:id', async (req, res) => {
     const updates = Object.keys(req.body)
     const allowedUpdates = ['name', 'email', 'password', 'age']
-    const isValid = updates.every((update) => allowedUpdates.includes(update))
-    if (!isValid)
-        return res.status(400).send({ error: 'Invalid property update' })
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid updates!' })
+    }
+
     try {
         const user = await User.findById(req.params.id)
+
         updates.forEach((update) => user[update] = req.body[update])
         await user.save()
-        if (!user)
+
+        if (!user) {
             return res.status(404).send()
+        }
+
         res.send(user)
     } catch (e) {
         res.status(400).send(e)
@@ -63,13 +75,15 @@ router.patch('/users/:id', async (req, res) => {
 })
 
 router.delete('/users/:id', async (req, res) => {
-    const _id = req.params.id
     try {
-        const user = await User.findByIdAndDelete(_id)
-        if (!user)
-            return res.status(404).send('user not found')
+        const user = await User.findByIdAndDelete(req.params.id)
+
+        if (!user) {
+            return res.status(404).send()
+        }
+
         res.send(user)
-    } catch {
+    } catch (e) {
         res.status(500).send()
     }
 })
