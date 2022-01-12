01456f9783ebe4c5a29c9e3739952e63f84fbd4d
guptajiakshat <akshatgupta2k@gmail.com>
2022-01-12 16:11:27 +0530

    auth

--- src/index.js
+++ src/index.js
@@ -6,10 +6,6 @@ const taskRouter = require('./router/task')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.use((req, res, next) => {
-    console.log(req.method, req.path)
-})
-
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
