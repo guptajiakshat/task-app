0b124f35a69dea24a59e2beb9cf9374567a6a789
guptajiakshat <akshatgupta2k@gmail.com>
2022-01-14 18:05:32 +0530

    paging done

--- src/index.js
+++ src/index.js
@@ -2,9 +2,20 @@ const express = require('express')
 require('./db/mongoose')
 const userRouter = require('./router/user')
 const taskRouter = require('./router/task')
-
 const app = express()
 const port = process.env.PORT || 3000
+
+
+
+const multer = require('multer')
+const upload = multer({
+    dest: 'images'
+})
+
+app.post('/upload', upload.single('upload'), (req, res) => {
+    res.send()
+})
+
 
 app.use(express.json())
 app.use(userRouter)
--- src/router/task.js
+++ src/router/task.js
@@ -8,6 +8,7 @@ router.post('/tasks', auth, async (req, res) => {
         ...req.body,
         owner: req.user._id
     })
+
     try {
         await task.save()
         res.status(201).send(task)
@@ -16,25 +17,49 @@ router.post('/tasks', auth, async (req, res) => {
     }
 })
 
+// get /tasks?completed=false
+// /tasks?limit=10&skip=5
+// /tasks?sortby=createdAt_asc/desc
+router.get('/tasks', auth, async (req, res) => {
+    const match = {}
+    const sort = {}
+    if (req.query.sortBy) {
+        const parts = req.query.sortBy.split('_')
+        sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
+    }
+    if (req.query.completed) {
+        if (req.query.completed == 'true')
+            match.completed = true
+        else
+            match.completed = false;
+    }
+    try {
+        // const tasks = await Task.find({ owner: req.user._id })
+        await req.user.populate({
+            path: 'tasks',
+            match,
+            options: {
+                limit: parseInt(req.query.limit),
+                skip: parseInt(req.query.skip),
+                sort
+            }
+        })
+        res.send(req.user.tasks)
+    } catch {
+        res.status(404).send()
+    }
+})
+
 router.get('/tasks/:id', auth, async (req, res) => {
     const _id = req.params.id
     try {
         const task = await Task.findOne({ _id, owner: req.user._id })
-        if (!task)
+        if (!task) {
             return res.status(404).send()
+        }
         res.send(task)
     } catch {
         res.status(500).send()
-    }
-})
-
-router.get('/tasks', auth, async (req, res) => {
-    try {
-        // const tasks = await Task.find({ owner: req.user._id })
-        await req.user.populate('tasks')
-        res.send(req.user.tasks)
-    } catch {
-        res.status(404).send()
     }
 })
 
