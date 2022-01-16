3a0c12b3cad3cf294fa2484f00a08d60a6e371dd
guptajiakshat <akshatgupta2k@gmail.com>
2022-01-16 14:11:13 +0530

    shark

--- src/index.js
+++ src/index.js
@@ -5,27 +5,6 @@ const taskRouter = require('./router/task')
 const app = express()
 const port = process.env.PORT || 3000
 
-
-
-const multer = require('multer')
-const upload = multer({
-    dest: 'images',
-    limits: {
-        fileSize: 1000000
-    },
-    // accept docs
-    fileFilter(req, file, cb) {
-        if (!file.originalname.match(/\.(doc|docx)$/)) {
-            return cb(new Error('please upload a word doc'))
-        }
-        cb(undefined, true)
-    }
-})
-app.post('/upload', upload.single('upload'), (req, res) => {
-    res.send()
-})
-
-
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
